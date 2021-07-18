76790c98e9aa340a5cbadf7a8632eb6eecd33246
Daniel Edgar <aknot242@gmail.com>
2021-07-18 07:54:16 -0700

    updated entrypoint

--- app-protect/entrypoint.sh
+++ app-protect/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-
-LOGPATH=/var/log/adm/admd.log
-
-# create shared cores if it missing before starting admd
-
 mkdir -p /shared/cores && chmod 755 /shared/cores
-/usr/bin/adminstall --daemons 1 --memory 100 > ${LOGPATH} 2>&1
-/usr/bin/admd -d --standalone > ${LOGPATH} 2>&1 &
+/usr/bin/adminstall --daemons 1 --memory 200 > /var/log/adm/admd.log 2>&1
 /bin/su -s /bin/bash -c '/opt/app_protect/bin/bd_agent &' nginx
 /bin/su -s /bin/bash -c "/usr/share/ts/bin/bd-socket-plugin tmm_count 4 proc_cpuinfo_cpu_mhz 2000000 total_xml_memory 307200000 total_umu_max_size 3129344 sys_max_account_id 1024 no_static_config 2>&1 > /var/log/app_protect/bd-socket-plugin.log &" nginx
+/bin/su -s /bin/bash -c '/usr/bin/admd -d --log info 2>&1 > /var/log/adm/admd.log &' nginx
 /usr/sbin/nginx -g 'daemon off;'
