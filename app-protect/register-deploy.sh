6ac9ca6b286635a9dfb33bee3fc405a50c913d8d
Daniel Edgar <aknot242@gmail.com>
2021-07-23 09:01:59 -0700

    add environment to deployment

--- app-protect/register-deploy.sh
+++ app-protect/register-deploy.sh
@@ -5,4 +5,4 @@ timestamp=`printf "%(%Y-%m-%dT%T)T" -1`
 hostname=`hostname`
 curl --location --request POST 'http://172.19.0.4:9200/app-deployment-logs/_doc' \
 --header 'Content-Type: application/json' \
---data-raw "{ \"@timestamp\": \"${timestamp}\", \"success\": true, \"applicationName\": \"dotnetcorewebapp\", \"targetHost\": \"${hostname}\" }"
+--data-raw "{ \"@timestamp\": \"${timestamp}\", \"success\": true, \"applicationName\": \"dotnetcorewebapp\", \"environmentName\": \"${PROXY_NAME}\", \"targetHost\": \"${hostname}\" }"
