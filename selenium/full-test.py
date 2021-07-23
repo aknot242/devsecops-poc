f08bde4ec11f617f6701d59d6afbceb3907d726b
Daniel Edgar <aknot242@gmail.com>
2021-07-23 07:20:49 -0700

    enable tests

--- selenium/full-test.py
+++ selenium/full-test.py
@@ -3,14 +3,14 @@ from seleniumbase import BaseCase
 
 class MyTestClass(BaseCase):
     def test_basics(self):
-        url = "http://edgar-devsecopspoc-staging.westus2.azurecontainer.io/"
+        url = "http://devsecops-poc-stage.28d5d6771fc84adba80b.westus2.aksapp.io/"
         self.open(url)
         self.assert_title("Home page - DotNetCoreWebApp")
         self.assert_exact_text("Welcome to DevSecOps", "h1")
         self.type('input[name="SearchString"]', "my search string")
         self.click('input[value="Search"]')
         # self.assert_text("xkcd: volume 0", "h3")
-        self.open("http://edgar-devsecopspoc-staging.westus2.azurecontainer.io/Privacy")
+        self.open("http://devsecops-poc-stage.28d5d6771fc84adba80b.westus2.aksapp.io/Privacy")
         self.assert_title("Privacy Policy - DotNetCoreWebApp")
         # self.assert_element('img[alt="Python"]')
         self.assert_exact_text("Privacy Policy", "h1")
