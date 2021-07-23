be88f51a92517a86dbebcce0b9541362ab032614
Daniel Edgar <aknot242@gmail.com>
2021-07-23 06:05:04 -0700

    use private network for logs and enable xff

--- app-protect/register-deploy.sh
+++ app-protect/register-deploy.sh
@@ -3,6 +3,6 @@
 #send deployment event to Elasticsearch
 timestamp=`printf "%(%Y-%m-%dT%T)T" -1`
 hostname=`hostname`
-curl --location --request POST 'http://edgar-docker.westus2.cloudapp.azure.com:9200/app-deployment-logs/_doc' \
+curl --location --request POST 'http://172.19.0.4:9200/app-deployment-logs/_doc' \
 --header 'Content-Type: application/json' \
 --data-raw "{ \"@timestamp\": \"${timestamp}\", \"success\": true, \"applicationName\": \"dotnetcorewebapp\", \"targetHost\": \"${hostname}\" }"
