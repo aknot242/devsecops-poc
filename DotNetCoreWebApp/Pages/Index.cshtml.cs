0006ca30dbedbbd05c485c016c3e67bfeeda729d
Daniel Edgar <aknot242@gmail.com>
2021-07-26 10:42:24 -0700

    move slow loading demo to new page

--- DotNetCoreWebApp/Pages/Index.cshtml.cs
+++ DotNetCoreWebApp/Pages/Index.cshtml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
@@ -22,7 +18,6 @@ namespace DotNetCoreWebApp.Pages
 
         public void OnGet()
         {
-            System.Threading.Thread.Sleep(1000);
         }
     }
 }
---
+++ DotNetCoreWebApp/Pages/Slow.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DotNetCoreWebApp.Pages
+{
+    public class SlowModel : PageModel
+    {
+        private readonly ILogger<SlowModel> _logger;
+
+        [BindProperty(SupportsGet = true)]
+        public string LoadTime { get; set; }
+
+        public SlowModel(ILogger<SlowModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public void OnGet()
+        {
+            Random rnd = new Random();
+            var time = rnd.Next(200, 2000);
+            System.Threading.Thread.Sleep(time);
+            this.LoadTime = time.ToString();
+        }
+    }
+}
