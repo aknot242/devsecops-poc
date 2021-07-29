b924d046380b5c729e2253459f42185a8ba7f1e1
Daniel Edgar <aknot242@gmail.com>
2021-07-29 07:53:05 -0700

    update page title in Selenium test

--- selenium/full-test.py
+++ selenium/full-test.py
@@ -6,11 +6,9 @@ class MyTestClass(BaseCase):
         url = "http://devsecops-poc-stage.28d5d6771fc84adba80b.westus2.aksapp.io/"
         self.open(url)
         self.assert_title("Home page - DotNetCoreWebApp")
-        self.assert_exact_text("Welcome to DevSecOps", "h1")
+        self.assert_exact_text("Welcome to DevSecOps at the NGINX Sprint 2.0 conference", "h1")
         self.type('input[name="SearchString"]', "my search string")
         self.click('input[value="Search"]')
-        # self.assert_text("xkcd: volume 0", "h3")
         self.open("http://devsecops-poc-stage.28d5d6771fc84adba80b.westus2.aksapp.io/Privacy")
         self.assert_title("Privacy Policy - DotNetCoreWebApp")
-        # self.assert_element('img[alt="Python"]')
         self.assert_exact_text("Privacy Policy", "h1")
