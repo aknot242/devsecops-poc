691ba3fb27b6c23434223228a7dcc95641903d93
Daniel Edgar <aknot242@gmail.com>
2022-03-15 13:01:38 -0700

    fix substitution

--- .github/scripts/register-deploy.sh
+++ .github/scripts/register-deploy.sh
@@ -3,7 +3,7 @@
 # send deployment event to Elasticsearch
 timestamp=`date "+%Y-%m-%dT%T%z"`
 url="${ELASTIC_URL}/app-deployment-logs/_doc"
-data="{ \"@timestamp\": \"${timestamp}\", \"success\": true, \"applicationName\": \"${APPLICATION_NAME}\", \"environmentName\": \"${ENVIRONMENT_NAME\" }"
+data="{ \"@timestamp\": \"${timestamp}\", \"success\": true, \"applicationName\": \"${APPLICATION_NAME}\", \"environmentName\": \"${ENVIRONMENT_NAME}\" }"
 echo sending "$data to $url"
 curl -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" --location --request POST "$url" \
 --header 'Content-Type: application/json' \
